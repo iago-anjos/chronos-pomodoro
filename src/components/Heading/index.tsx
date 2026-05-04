f29e7b0956c6bd9e4093b51342b038a9e2b158b9
Iago Anjos <iagokaued@gmail.com>
2026-05-04 15:58:57 -0300

    feature: add Logo and Menu

---
+++ src/components/Heading/index.tsx
@@ -0,0 +1,14 @@
+import type React from "react";
+import styles from "./styles.module.css";
+
+type HeadingProps = {
+  children: React.ReactNode;
+};
+
+export function Heading(props: HeadingProps) {
+  return (
+    <>
+      <h1 className={styles.heading}>{props.children}</h1>
+    </>
+  );
+}
